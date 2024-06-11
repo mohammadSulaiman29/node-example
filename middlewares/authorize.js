d3ecfcfc4724994aae12da524b6ffe1eb92efcac
mohammadsulaiman29 <mohammadsulaiman355@github.com>
2024-06-11 14:18:39 +0300

    Request query

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -2,8 +2,8 @@ const authorize = (req , res , next) => {
     console.log("Authorize middleware");
     const {user} = req.query;
     if(user === 'Mohammad'){
-        req.query.user = {name : "Mohammad" , id : 12 };
-        console.log( req.query.user);
+        req.user = {name : "Mohammad" , id : 12 };
+        console.log( req.user);
         next();
     }
     else{
