27638965cda69f5c077542fcae4d3ae2257711aa
mohammadsulaiman29 <mohammadsulaiman355@github.com>
2024-06-11 13:55:44 +0300

    Add router

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const expree = require('express');
 const path = require('path');
-// const db = require('./config/database');
-
 const app = expree();
+// user routes
+const {userRouter} = require('./routes/user.js');
+
+// explain process
 const PORT = process.env.PORT || 8000;
 
 app.get('/' , (req , res) => {
     res.send("Home page");
 });
 
+app.use('/users' , userRouter);
 app.listen(PORT , () => {
     console.log(`Server starting on port ${PORT}`);
 });
---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const userRouter = express.Router();
+
+userRouter.get('/' , (req , res) => {
+    res.status(200).send("Users");
+    console.log("users");
+});
+
+userRouter.get('/:id' , (req , res) => {
+    res.status(200).send(`User by id ${req.params.id}`);
+    console.log(`users with ID : ${req.params.id}`);
+});
+
+userRouter.get('/create' , (req , res) => {
+    console.log("Create Form");
+});
+
+userRouter.post('/store' , (req ,res) => {
+    console.log("Store");
+});
+
+userRouter.delete('/delete/:id' , (req , res) => {
+    console.log(`Delete user ${req.params.id}`);
+});
+
+module.exports = {userRouter};
