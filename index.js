5c552ecb9e7df4196daafaf5850becdf0512f45b
mohammadsulaiman29 <mohammadsulaiman355@github.com>
2024-06-12 11:19:40 +0300

    Add delete method with operations

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(express.static('./methods-public'));
 
 // And we have a third party middleware like morgan (npm)
 
+// this parser middleware to handle the JSON data (important)
+app.use(express.json());
 // parse form data 
 app.use(express.urlencoded({ extended: false }));
 // explain process
@@ -43,37 +45,54 @@ app.post('/login', (req, res) => {
     }
 });
 
-app.post('/api/postman/people' , (req , res) => {
-    const {name} = req.body;
-    if(name){
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body;
+    if (name) {
         return res.status(200).json({
-            success : true , 
-            msg : "Hello",
+            success: true,
+            msg: "Hello",
         })
     }
     res.status(200).json({
-        success : true,
-        data : [...peoples , name],
+        success: true,
+        data: [...peoples, name],
     })
 });
 
-app.put('/edit/user/:id' , (req , res) => {
-    res.status(200).json({
-        status : true,
-        data : {
-            id : req.params.id,
-            name : req.body.name
+app.put('/edit/user/:id', (req, res) => {
+    const { name } = req.body;
+    const { id } = req.params;
+    const person = peoples.find((person) => person.id === Number(id));
+    if (!person) {
+        return res
+            .status(404)
+            .json({ success: false, msg: `No person for id ${id}` });
+    }
+    const newPeople = peoples.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name;
         }
+        return person;
     });
+    res
+        .status(200)
+        .json({ success: true, data: newPeople });
 });
 
-app.delete('/delete/user/:id' , (req , res) => {
-    const {name} = req.body;
-    res.status(200).json({
-        status : true,
-        id : req.params.id,
-        name : name,
-    })
+app.delete('/delete/user/:id', (req, res) => {
+    const { id } = req.params;
+    const person = peoples.find((person) => person.id === Number(id));
+    if (!person) {
+        return res
+            .status(404)
+            .json({ success: false, msg: `No person for id ${id}` });
+    };
+    const newPeople = peoples.filter((person) => {
+        return person.id !== Number(id);
+    });
+    res
+        .status(200)
+        .json({ success: true, data: newPeople });
 });
 
 app.use('/users', userRouter);
